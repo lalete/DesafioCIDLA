73a906c702dd40baf5ca7c5665b69510bc641275
Nicolás <Ncollio2016@alu.uct.cl>
2020-10-13 19:03:32 -0300

    pruebas
    test usando pymongo

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ db = client['pppprueba']
 collection = db['fichas']
 
 collection.insert_one({"nombre": "miguel" , "edad": 20})
+
+results = collection.find()                 #funcion para buscar una coleccion en la base de datos.
+
+for r in results:                           #recorre results buscando todos los archivos con la casilla nombre.
+    print(r['nombre'])
+
+#collection.delete_many(["nombre":miguel ]) sirve para borrar a todos los archivos con nombre de miguel.
+#collection.delete_one() para borrar 1 archivo.
+
+numero_de_fichas = collection.count_documents({})  #sirve para contar el numero de fichas de la base de datos.
